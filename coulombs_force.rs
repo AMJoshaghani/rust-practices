5eb1536142fd44a97d7318bde8ee9edd859db26d
Amir M. Joshaghani <46087303+AMJoshaghani@users.noreply.github.com>
2024-12-24 19:59:45 +0330

    Update coulombs_force.rs

--- coulombs_force.rs
+++ coulombs_force.rs
@@ -7,12 +7,15 @@ const K: f64 = 1f64 / (4f64 * PI * E_0); // Coulomb's Constant, approx. value: +
 fn main() {
     // F = 1/(4 . pi . E_0) . (q q) . r / |r^3|
     let q: f64 = 1E-6; // Electric Charge in Coulomb
-    let r: Vector3D = Vector3D::new(1f64,2f64,3f64); // Separation Vector
-    let f = r.scalar_product(&(K * (q * q)))
-        .scalar_product(&(1f64 / r.magnitude().powf(3f64))); // Coulomb's Force
+    let r1: Vector3D = Vector3D::new(0.0, 0.0, 0.0); // OQ: q position vector
+    let r2: Vector3D = Vector3D::new(1.0, 2.0, 3.0); // OQ': q' position vector
+    let r_prime: Vector3D = r1.separation_with(&r2); // Separation Vector for OQ and OQ'
+    let f = r_prime.scalar_product(&(K * (q * q)))
+        .scalar_product(&(1f64 / r_prime.magnitude().powf(3f64))); // Coulomb's Force
     println!("{:?}", f); //: Vector3D { x: 0.0001715731561264385, y: 0.000343146312252877, z: 0.0005147194683793155 }
     println!("{}", f.magnitude()) //: 0.0006419679669926073
 }
+
 mod vector {
     #[derive(Debug)]
     pub struct Vector3D {
@@ -22,7 +25,7 @@ mod vector {
     }
     impl Vector3D {
         pub fn new(x: f64, y: f64, z: f64) -> Self {
-            Vector3D {
+            Self {
                 x,
                 y,
                 z
@@ -40,9 +43,9 @@ mod vector {
         //         z: self.x * vec2.y - self.y * vec2.x
         //     }
         // }
-        pub fn scalar_product(&self, s: &f64) -> Vector3D
+        pub fn scalar_product(&self, s: &f64) -> Self
         {
-            Vector3D {
+            Self {
                 x: s * self.x,
                 y: s * self.y,
                 z: s * self.z
@@ -51,6 +54,13 @@ mod vector {
         pub fn magnitude(&self) -> f64 {
             // ((self.x.powf(2.)) + (self.y.powf(2.)) + (self.z.powf(2.))).powf(0.5)
             self.dot_product(self).powf(0.5) // |r| = sqrt( r . r )
+        }
+        pub fn separation_with(&self, vec2: &Vector3D) -> Self {
+            Self {
+                x: vec2.x - self.x,
+                y: vec2.y - self.y,
+                z: vec2.z - self.z,
+            }
         }
     }
 }
